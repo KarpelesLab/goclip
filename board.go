edc497df2af33efd416a9a56250e017f00a87d3e
Mark Karpeles <magicaltux@gmail.com>
2020-11-17 02:41:56 +0900

    rename type→board, add no board error

--- board.go
+++ board.go
@@ -3,7 +3,7 @@ package goclip
 type Board uint8
 
 const (
-	InvaliBoard        Board = iota
+	InvalidBoard       Board = iota
 	Default                  // the default clipboard
 	PrimarySelection         // the primary selection (X11 only)
 	SecondarySelection       // the secondary selection (X11 only)
--- error.go
+++ error.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrFormatUnavailable = errors.New("goclip: requested format was not available")
+	ErrNoBoard           = errors.New("goclip: requested board is not available")
 )
